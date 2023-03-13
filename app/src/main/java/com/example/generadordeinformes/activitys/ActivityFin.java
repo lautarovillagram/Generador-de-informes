eac2723884a430334e40bb383f0c7f98c42f8d2f
LautaroVillagraM <lautarovillagram@gmail.com>
2023-03-13 19:23:51 -0300

    activitys generadas y clase informe

---
+++ app/src/main/java/com/example/generadordeinformes/activitys/ActivityFin.java
@@ -0,0 +1,16 @@
+package com.example.generadordeinformes.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.generadordeinformes.R;
+
+public class ActivityFin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fin);
+    }
+}
---
+++ app/src/main/java/com/example/generadordeinformes/clases/InicioOperativo.java
@@ -0,0 +1,100 @@
+package com.example.generadordeinformes.clases;
+
+public class InicioOperativo {
+    private String base;
+    private String tipoOperativo;
+    private String lugarOperativo;
+    private String horarioInicio;
+    private String horarioFin;
+    private String agentes;
+    private String dominios;
+    private String fuerza;
+    private String observaciones;
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getTipoOperativo() {
+        return tipoOperativo;
+    }
+
+    public void setTipoOperativo(String tipoOperativo) {
+        this.tipoOperativo = tipoOperativo;
+    }
+
+    public String getLugarOperativo() {
+        return lugarOperativo;
+    }
+
+    public void setLugarOperativo(String lugarOperativo) {
+        this.lugarOperativo = lugarOperativo;
+    }
+
+    public String getHorarioInicio() {
+        return horarioInicio;
+    }
+
+    public void setHorarioInicio(String horarioInicio) {
+        this.horarioInicio = horarioInicio;
+    }
+
+    public String getHorarioFin() {
+        return horarioFin;
+    }
+
+    public void setHorarioFin(String horarioFin) {
+        this.horarioFin = horarioFin;
+    }
+
+    public String getAgentes() {
+        return agentes;
+    }
+
+    public void setAgentes(String agentes) {
+        this.agentes = agentes;
+    }
+
+    public String getDominios() {
+        return dominios;
+    }
+
+    public void setDominios(String dominios) {
+        this.dominios = dominios;
+    }
+
+    public String getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(String fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public InicioOperativo(String base, String tipoOperativo, String lugarOperativo, String horarioInicio,
+                           String horarioFin, String agentes, String dominios, String fuerza, String observaciones) {
+        this.base = base;
+        this.tipoOperativo = tipoOperativo;
+        this.lugarOperativo = lugarOperativo;
+        this.horarioInicio = horarioInicio;
+        this.horarioFin = horarioFin;
+        this.agentes = agentes;
+        this.dominios = dominios;
+        this.fuerza = fuerza;
+        this.observaciones = observaciones;
+    }
+}
+
+
